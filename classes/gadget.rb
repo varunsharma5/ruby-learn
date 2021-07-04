66e2beba4f381daa670d60c0dd7f64e47f008a64
Varun Sharma <varun.sharma@progress.com>
2021-07-04 20:01:03 +0530

    Initial Commit

---
+++ classes/gadget.rb
@@ -0,0 +1,7 @@
+class Gadget
+
+end
+
+phone = Gadget.new
+laptop = Gadget.new
+microwave = Gadget.new
---
+++ classes/methods.rb
@@ -0,0 +1,8 @@
+arr_int_methods = p 5.methods.sort
+arr_float_methods = 2.2.methods.sort
+
+p arr_float_methods - arr_int_methods
+puts
+p arr_int_methods - arr_float_methods
+puts
+p arr_int_methods & arr_float_methods
