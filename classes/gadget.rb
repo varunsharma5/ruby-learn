079a88c672b523d351856495cfd002aec9f90d70
Varun Sharma <varun.sharma@progress.com>
2021-07-04 20:14:47 +0530

    checkin-in methods

--- classes/gadget.rb
+++ classes/gadget.rb
@@ -5,3 +5,17 @@ end
 phone = Gadget.new
 laptop = Gadget.new
 microwave = Gadget.new
+
+puts phone
+puts laptop
+puts microwave
+
+puts phone.is_a?(Gadget)
+puts phone.is_a?(Object)
+puts phone.is_a?(BasicObject)
+
+puts
+
+puts phone.respond_to?(:class)
+puts phone.respond_to?(:is_a?)
+puts phone.respond_to?(:length)
