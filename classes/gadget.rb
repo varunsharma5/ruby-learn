042fa5cf152d4d2e70cb0fe876cd37430f4c1415
Varun Sharma <varun.sharma@progress.com>
2021-07-07 15:34:25 +0530

    getter methods

--- classes/gadget.rb
+++ classes/gadget.rb
@@ -4,6 +4,15 @@ class Gadget
         @password = "secret"
         @production_number = "#{("a".."z").to_a.sample}-#{rand(1...999)}"
     end
+    
+    # Getter Methods
+    def username
+        @username
+    end
+
+    def protected_number
+        @production_number
+    end
 
     def to_s
         puts "User: #{@username}; Production Number: #{@production_number}. Its is made from the #{self.class} and it has ID #{self.object_id}"
@@ -14,5 +23,7 @@ phone = Gadget.new
 laptop = Gadget.new
 
 
-puts phone.to_s
-puts laptop.to_s
+# puts phone.to_s
+# puts laptop.to_s
+puts phone.username
+puts phone.protected_number
